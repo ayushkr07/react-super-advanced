91fb5c254b7ad99356953667c69f18b5f3749923
Ayush Kumar <aks010598@gmail.com>
2021-07-05 05:39:26 +0530

    without jsx

--- src/2.jsx/Hello.js
+++ src/2.jsx/Hello.js
@@ -1,1 +1,19 @@
 
+import React from 'react'
+
+// function Hello() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+
+// without using JSX
+// const Hello = () => {
+//     return React.createElement('div',null,
+//     React.createElement('h1',null,'without using jsx'))
+// }
+
+// export default Hello
